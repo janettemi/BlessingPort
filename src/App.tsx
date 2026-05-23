4b76b232982c85fd82fdbb3bd0eaace5c33434de
unknown <ogunnaikejanettemitope@gmail.com>
2026-05-23 05:41:59 -0700

    commented app.js out

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 
-import Hero from './components/Hero';
-import About from './components/About';
-import Logos from './components/Logos';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import BottomNav from './components/BottomNav';
+// import Hero from './components/Hero';
+// import About from './components/About';
+// import Logos from './components/Logos';
+// import Portfolio from './components/Portfolio';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import BottomNav from './components/BottomNav';
 
 export default function App() {
   return (
     <div className="min-h-screen selection:bg-primary-container selection:text-primary overflow-x-hidden">
-      <Hero />
+      {/* <h1> hello world</h1> */}
+      {/* <Hero />
       <About />
        <Logos />
       <Portfolio />
       <Contact />
       <Footer />
-      <BottomNav />
+      <BottomNav /> */}
     </div>
   );
 }
